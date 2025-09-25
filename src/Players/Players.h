cf87bcf17448a9922760d429fa28636f097ee6ca
ThorNoHeim <tnn717@gmail.com>
2025-09-25 10:37:23 +0200

    Chose which was correct

--- src/Players/Players.h
+++ src/Players/Players.h
@@ -1,55 +1,55 @@
-//#pragma once
-//#include <raylib.h>
-//#include <string>
-//#include <iostream>
-//#include <algorithm>
-//#include <cmath>
-//#include <vector>
-//
-//class Player
-//{
-//public:
-//
-//	// Global Variables
-//	float windowWidth = 1800;
-//	float windowHeight = 950;
-//	float windowHalfWidth = windowWidth / 2;
-//	float windowHalfHeight = windowHeight / 2;
-//
-//	// Global Player Variables
-//	float playerXPosition = windowHalfWidth - 870;
-//	float playerYPosition = windowHalfHeight - 27;
-//	float player2XPosition = windowHalfWidth - 870;
-//	float player2YPosition = windowHalfHeight + 27;
-//
-//	Color playerColor{ WHITE };
-//	Color player2color{ WHITE };
-//
-//	//constructor
-//	void DrawPlayer(Color inPlayerColor);
-//	void DrawPlayer2(Color inPlayer2Color);
-//
-//	//functions
-//	void PlayerController();
-//
-//	//bool IsPlayerInBounds()
-//	//{
-//	//	bool withinXBounds = playerXPosition >= 0 || playerXPosition <= windowWidth ? true : false;
-//
-//	//	bool withinYBounds = playerYPosition >= 0 || playerYPosition <= windowHeight ? true : false;
-//
-//	//	return withinXBounds && withinYBounds;
-//	//}
-//
-//	void Player2Controller();
-//
-//	//	bool IsPlayer2InBounds()
-//	//	{
-//	//		bool within2XBounds = player2Xposition >= 0 || player2Xposition <= windowWidth ? true : false;
-//	//
-//	//		bool within2YBounds = player2Yposition >= 0 || player2Yposition <= windowHeight ? true : false;
-//	//
-//	//		return within2XBounds && within2YBounds;
-//	//	}
-//	//};
-//};
+#pragma once
+#include <raylib.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+class Player
+{
+public:
+
+	// Global Variables
+	float windowWidth = 1800;
+	float windowHeight = 950;
+	float windowHalfWidth = windowWidth / 2;
+	float windowHalfHeight = windowHeight / 2;
+
+	// Global Player Variables
+	float playerXPosition = windowHalfWidth - 870;
+	float playerYPosition = windowHalfHeight - 27;
+	float player2XPosition = windowHalfWidth - 870;
+	float player2YPosition = windowHalfHeight + 27;
+
+	Color playerColor{ WHITE };
+	Color player2color{ WHITE };
+
+	//constructor
+	void DrawPlayer(Color inPlayerColor);
+	void DrawPlayer2(Color inPlayer2Color);
+
+	//functions
+	void PlayerController();
+
+	//bool IsPlayerInBounds()
+	//{
+	//	bool withinXBounds = playerXPosition >= 0 || playerXPosition <= windowWidth ? true : false;
+
+	//	bool withinYBounds = playerYPosition >= 0 || playerYPosition <= windowHeight ? true : false;
+
+	//	return withinXBounds && withinYBounds;
+	//}
+
+	void Player2Controller();
+
+	//	bool IsPlayer2InBounds()
+	//	{
+	//		bool within2XBounds = player2Xposition >= 0 || player2Xposition <= windowWidth ? true : false;
+	//
+	//		bool within2YBounds = player2Yposition >= 0 || player2Yposition <= windowHeight ? true : false;
+	//
+	//		return within2XBounds && within2YBounds;
+	//	}
+	//};
+};
